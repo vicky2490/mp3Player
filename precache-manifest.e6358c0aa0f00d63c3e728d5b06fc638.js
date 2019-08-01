d5ac3cf6f6f3c6be6f61271604ecf5576a81d6ea
vicky <s353397@sinyi.com.tw>
2019-08-01 15:42:14 +0800

    Updates

---
+++ precache-manifest.e6358c0aa0f00d63c3e728d5b06fc638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8323e5528f9c3eefa82db64f9f57ae3c",
+    "url": "/mp3player/index.html"
+  },
+  {
+    "revision": "a7442e0961f7536dfdca",
+    "url": "/mp3player/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mp3player/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a7442e0961f7536dfdca",
+    "url": "/mp3player/static/js/main.d57a6f17.chunk.js"
+  },
+  {
+    "revision": "137edc290ca4caa88b4c",
+    "url": "/mp3player/static/js/runtime~main.ff16f472.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mp3player/static/media/logo.5d5d9eef.svg"
+  }
+]);
