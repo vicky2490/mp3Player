279e04aed746b5684473aacda93ab65639b0c763
vicky2490 <s353397@sinyi.com.tw>
2019-08-04 00:10:35 +0800

    do list box and content shadow right

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="wrap">
+      <div className="box">
+        <div className="list-box"></div>
+        <div className="list-box flex-column">
+          <div className="list-word ">List</div>
+          <div className="list-line"></div>
+          <div className="list-line"></div>
+          <div className="list-line"></div>
+        </div>
+      </div>
       <div className="content">
-        <div className="rec rec-btn"></div>
-        <div className="rec rec-right2"></div>
+        <div className="rec rec-bottom"></div>
+        <div className="rec rec-right-border">
+          <div className="rec rec-right"></div>                    
+        </div>
+        
       </div>
     </div>
   );
