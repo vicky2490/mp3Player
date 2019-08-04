2f98df3bea04b729074ce3fcefde381cc805afdb
vicky2490 <s353397@sinyi.com.tw>
2019-08-04 21:21:26 +0800

    add pic and control btn

--- src/App.js
+++ src/App.js
@@ -14,11 +14,30 @@ function App() {
         </div>
       </div>
       <div className="content">
+        <div className="upper-half">
+          <div className="pic">
+            <div className="circle"></div>
+          </div>
+          <div className="right-half-btn">
+            <div className="control-btn">
+              <div className="btn-gray"></div> 
+              <div className="btn previous"></div>
+            </div>
+            <div className="control-btn margin-auto">
+              <div className="btn-gray"></div> 
+              <div className="btn next"></div>
+            </div>
+            <div className="control-btn">
+              <div className="btn-gray"></div> 
+              <div className="btn shuffle-mode"></div>
+            </div>
+          </div>
+        </div>
+        <div className="control"></div>
         <div className="rec rec-bottom"></div>
         <div className="rec rec-right-border">
           <div className="rec rec-right"></div>                    
-        </div>
-        
+        </div>  
       </div>
     </div>
   );
